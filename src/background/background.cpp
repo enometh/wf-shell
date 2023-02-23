7e949d1ff723fce89ed9d65c4e4196d09e916222
Madhu <enometh@net.meer>
2023-02-23 15:22:30 +0530

    wf-background: monitor background image and reload if changed
    [230515]
    * src/background/background.cpp: (change_background): respool file_monitor on failure via set_background

--- src/background/background.cpp
+++ src/background/background.cpp
@@ -298,6 +298,7 @@ bool WayfireBackground::change_background()
     auto next_image = load_next_background();
     if (!next_image)
     {
+        set_background();
         return false;
     }
 
@@ -416,11 +417,85 @@ void WayfireBackground::reset_background()
     change_bg_conn.disconnect();
 }
 
+#define GIOMM_24
+#undef GIOMM_24
+static Glib::RefPtr<Gio::Cancellable> cancellable;
+
+void WayfireBackground::file_monitor(std::string& path)
+{
+    if (tag)
+    {
+        tag.disconnect();
+    }
+
+    Glib::RefPtr<Gio::File> gf = Gio::File::create_for_path(path.c_str());
+    Gio::FileType ft = gf->query_file_type();
+    try {
+        if (ft ==
+#ifdef GIOMM_24
+            Gio::FILE_TYPE_DIRECTORY
+#else
+            Gio::FileType::DIRECTORY
+#endif
+        )
+        {
+            fm = gf->monitor_directory(cancellable,
+#ifdef GIOMM_24
+                Gio::FILE_MONITOR_NONE
+#else
+                Gio::FileMonitorFlags::NONE
+#endif
+            );
+        } else
+        {
+            fm = gf->monitor_file(cancellable,
+#ifdef GIOMM_24
+                Gio::FILE_MONITOR_NONE
+#else
+                Gio::FileMonitorFlags::NONE
+#endif
+            );
+        }
+    } catch (const Gio::Error& error)
+    {
+        std::cerr << "g_file_monitor_file(" << path <<
+            ": failed: " << error.what() << std::endl;
+        return;
+    }
+
+    tag = fm->signal_changed().connect([this] (const Glib::RefPtr<Gio::File>& file,
+                                               const Glib::RefPtr<Gio::File>& other_file,
+#ifdef GIOMM_24
+                                               Gio::FileMonitorEvent
+#else
+                                               Gio::FileMonitor::Event
+#endif
+                                               event)
+    {
+        if (
+#ifdef GIOMM_24
+            event != Gio::FILE_MONITOR_EVENT_CHANGES_DONE_HINT &&
+            event != Gio::FILE_MONITOR_EVENT_DELETED
+
+#else
+            event != Gio::FileMonitor::Event::CHANGES_DONE_HINT &&
+            event != Gio::FileMonitor::Event::DELETED
+#endif
+        )
+        {
+            return;
+        }
+
+        set_background();
+    });
+}
+
 void WayfireBackground::set_background()
 {
     reset_background();
 
     std::string path = background_image;
+    file_monitor(path);
     try {
         if (load_images_from_dir(path) && images.size())
         {
